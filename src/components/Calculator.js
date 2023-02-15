c57546cf8d84a72ed43a801093822fa47c809518
Chukwuemeka Kanu <mekuseo@gmail.com>
2023-02-15 11:51:31 +0100

    Implemented Events

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,21 +1,35 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import './UI.css';
+import calculate from './logic/Calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
-    super();
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      // operation: null,
+    };
+  }
+
+  handleClick = (event) => {
+    const { value } = event.target;
+    this.setState((prevState) => calculate(prevState, value));
   }
 
   render() {
+    const { total, next } = this.state;
+    const displayValue = next || total || '0';
     return (
       <div className="calculator">
-        <div className="calculator__display">0</div>
+        <div className="calculator__display">{displayValue}</div>
         <div className="calculator__keys">
           <button
             type="submit"
             className="calculator__key calculator__key--operator"
             value="AC"
+            onClick={this.handleClick}
           >
             AC
           </button>
@@ -23,6 +37,7 @@ class Calculator extends React.Component {
             type="submit"
             className="calculator__key calculator__key--operator"
             value="+/-"
+            onClick={this.handleClick}
           >
             +/-
           </button>
@@ -30,6 +45,7 @@ class Calculator extends React.Component {
             type="submit"
             className="calculator__key calculator__key--operator"
             value="%"
+            onClick={this.handleClick}
           >
             %
           </button>
@@ -37,54 +53,58 @@ class Calculator extends React.Component {
             type="submit"
             className="calculator__key calculator__key--operator"
             value="/"
+            onClick={this.handleClick}
           >
-            &divide;
+            /
           </button>
-          <button type="submit" className="calculator__key" value="7">
+          <button type="submit" className="calculator__key" value="7" onClick={this.handleClick}>
             7
           </button>
-          <button type="submit" className="calculator__key" value="8">
+          <button type="submit" className="calculator__key" value="8" onClick={this.handleClick}>
             8
           </button>
-          <button type="submit" className="calculator__key" value="9">
+          <button type="submit" className="calculator__key" value="9" onClick={this.handleClick}>
             9
           </button>
           <button
             type="submit"
             className="calculator__key calculator__key--operator"
-            value="X"
+            value="*"
+            onClick={this.handleClick}
           >
-            X
+            *
           </button>
-          <button type="submit" className="calculator__key" value="4">
+          <button type="submit" className="calculator__key" value="4" onClick={this.handleClick}>
             4
           </button>
-          <button type="submit" className="calculator__key" value="5">
+          <button type="submit" className="calculator__key" value="5" onClick={this.handleClick}>
             5
           </button>
-          <button type="submit" className="calculator__key" value="6">
+          <button type="submit" className="calculator__key" value="6" onClick={this.handleClick}>
             6
           </button>
           <button
             type="submit"
             className="calculator__key calculator__key--operator"
             value="-"
+            onClick={this.handleClick}
           >
             -
           </button>
-          <button type="submit" className="calculator__key" value="1">
+          <button type="submit" className="calculator__key" value="1" onClick={this.handleClick}>
             1
           </button>
-          <button type="submit" className="calculator__key" value="2">
+          <button type="submit" className="calculator__key" value="2" onClick={this.handleClick}>
             2
           </button>
-          <button type="submit" className="calculator__key" value="3">
+          <button type="submit" className="calculator__key" value="3" onClick={this.handleClick}>
             3
           </button>
           <button
             type="submit"
             className="calculator__key calculator__key--operator"
             value="+"
+            onClick={this.handleClick}
           >
             +
           </button>
@@ -92,6 +112,7 @@ class Calculator extends React.Component {
             type="submit"
             className="calculator__key calculator__key--zero"
             value="0"
+            onClick={this.handleClick}
           >
             0
           </button>
@@ -99,6 +120,7 @@ class Calculator extends React.Component {
             type="submit"
             className="calculator__key calculator__key--decimal"
             value="."
+            onClick={this.handleClick}
           >
             .
           </button>
@@ -106,6 +128,7 @@ class Calculator extends React.Component {
             type="submit"
             className="calculator__key calculator__key--equal"
             value="="
+            onClick={this.handleClick}
           >
             =
           </button>
